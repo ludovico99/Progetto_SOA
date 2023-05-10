212db3ac7b6c24ba8925d7ae805926bd6fdadc7e
ludovico99 <ludovico.zarr@gmail.com>
2023-05-10 17:41:57 +0200

    concurrency test

--- user_data_management_project/user/user.c
+++ user_data_management_project/user/user.c
@@ -28,9 +28,9 @@ void *multi_ops(void *index)
 
     // 66 % READERS, 33% WRITERS
 
-    if (my_id <= (NUM_THREADS * 2) / 3)
+    if (my_id < (NUM_THREADS * 2) / 3)
         arg = GET_DATA;
-    else if (my_id >= (NUM_THREADS * 2) / 3 + NUM_THREADS / 6)
+    else if (my_id > (NUM_THREADS * 2) / 3 + NUM_THREADS / 6)
         arg = PUT_DATA;
     else
         arg = INVALIDATE_DATA;
